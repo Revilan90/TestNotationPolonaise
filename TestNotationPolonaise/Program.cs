dc2658eccd73ad519c017251e1ee3b76490a4fb0
Revilan90 <johannesmassiani@yahoo.fr>
2022-02-25 10:12:36 +0100

    ça a l'air de fonctionner

--- TestNotationPolonaise/Program.cs
+++ TestNotationPolonaise/Program.cs
@@ -27,6 +27,63 @@ namespace TestNotationPolonaise
             } while (reponse != carac1 && reponse != carac2);
             return reponse;
         }
+        static string Polonaise(string form)
+        {
+            string erreur = "NaN";
+            float resultat;
+            string addition = "resultat = *4+2;";
+            try
+            {
+                /*ce que j'envisage c'est de casser la formule en petits poceau grace à .Split
+                 * puis on sépare les parties signes (/ + * etc) et les chiffres
+                 * ensuite on applique les formule en utilisant des if tel que if(tableau[x]==/) alors valeur1 / valeur2
+                 * 
+                 * on part de la fin du tableau et on revient vers la gauche, on cherche les signes, quand on en trouve un on prend les 2 cases à droite, on fait l'opération des deux case par le symbole
+                 * */
+                string[] tableau = form.Split(' '), signe;
+                int taille = tableau.Length, z;
+                for (int y = taille - 1; y >= 0; y--)
+                {
+                    if (tableau[y] == "+" || tableau[y] == "-" || tableau[y] == "*" || tableau[y] == "/")
+                    {
+                        switch (tableau[y])
+                        {
+                            case "+":
+                                tableau[y] = (float.Parse(tableau[y + 1]) + float.Parse(tableau[y + 2])).ToString();
+                                break;
+                            case "-":
+                                tableau[y] = (float.Parse(tableau[y + 1]) - float.Parse(tableau[y + 2])).ToString();
+                                break;
+                            case "*":
+                                tableau[y] = (float.Parse(tableau[y + 1]) * float.Parse(tableau[y + 2])).ToString();
+                                break;
+                            case "/":
+                                tableau[y] = (float.Parse(tableau[y + 1]) / float.Parse(tableau[y + 2])).ToString();
+                                break;
+                        }
+                        z = y;
+                        while ((z + 3) < taille)
+                        {
+                            tableau[z + 1] = tableau[z + 3];
+
+                            z++;
+                        }
+                        tableau[z + 2] = "";
+                        tableau[z + 1] = "";
+                    }
+                }
+                resultat = float.Parse(tableau[0]);
+                if (tableau[0] == "∞")
+                {
+                    return erreur;
+                }
+                return tableau[0];
+            }
+            catch
+            {
+                return erreur;
+            }
+        }
 
         /// <summary>
         /// Saisie de formules en notation polonaise pour tester la fonction de calcul
